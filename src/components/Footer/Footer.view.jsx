ce37b1f1df1b1e096df4a4b7b7884b291b5312fb
mohamedabusrea <mohamedabusrea@gmail.com>
2020-09-08 08:01:01 +0200

    Disable link in the footer for now

--- src/components/Footer/Footer.view.jsx
+++ src/components/Footer/Footer.view.jsx
@@ -6,10 +6,10 @@ const Footer = () => {
   return (
     <footer className='Footer'>
       <span>يمكنك مشاهدة خطوات بناء هذا المشروع في </span>
-      <a className='Footer__link'
-         href="#"
-         target='_blank'
-         rel="noopener noreferrer">هذا الفيديو</a>
+      {/*<a className='Footer__link'*/}
+      {/*   href="#"*/}
+      {/*   target='_blank'*/}
+      {/*   rel="noopener noreferrer">هذا الفيديو</a>*/}
     </footer>
   );
 }
